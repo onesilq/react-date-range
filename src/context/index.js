d90b845b39d51ce46217e1a7082f02780470eb46
eswarclynn <esuclintblazer@gmail.com>
2025-05-15 07:00:59 +0530

    revert: delete unused context

--- src/context/index.js
+++
@@ -1,7 +0,0 @@
-import { createContext } from 'react';
-
-export const DateRangeContext = createContext({});
-
-export const DateRangeProvider = ({ children }) => {
-  return <DateRangeContext.Provider value={{}}>{children}</DateRangeContext.Provider>;
-};
